e1652eba3a0abe15b74377a54a913bc6cfd9c386
Alexzander Avila <alexzanderburden@gmail.com>
2026-04-29 23:51:36 -0700

    Updated Weather Exercise to use ViewComponents.

---
+++ WeatherExcercise/WeatherExcercise/Components/WeatherNodeViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherExcercise.Models;
+namespace WeatherExcercise.Components
+{
+    public class WeatherNodeViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather city)
+        {
+            return View(city);
+        }
+    }
+}
--- WeatherExcercise/WeatherExcercise/obj/Debug/net10.0/WeatherExcercise.AssemblyInfo.cs
+++ WeatherExcercise/WeatherExcercise/obj/Debug/net10.0/WeatherExcercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherExcercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a7e37a5f5530b90a794d43892dc6c6e3aaf914")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3b37a0974e292b14ac5d1c13805fb72bf8c729")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherExcercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherExcercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
